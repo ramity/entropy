ee076ff37ca0177d23b7c8eb2443c2f235ab6fce
ramity <lewisclbrown@gmail.com>
2025-03-13 00:10:10 +0000

    init framework and container setup

---
+++ scripts/copy-env.bash
@@ -0,0 +1,5 @@
+cp -u docker/cache/.env.dist docker/cache/.env
+cp -u docker/core-backend/.env.dist docker/core-backend/.env
+cp -u docker/database/.env.dist docker/database/.env
+cp -u docker/database-viewer/.env.dist docker/database-viewer/.env
+cp -u docker/notification-backend/.env.dist docker/notification-backend/.env
